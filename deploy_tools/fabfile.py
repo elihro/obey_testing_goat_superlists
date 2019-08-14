4ea6abd25ee19b10a8f3f26932a22de38d381e04
Jairo Duarte Rodelo <elihro.duarte@gmail.com>
2019-08-13 20:20:23 -0500

    Added env.user and env.host in fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -3,6 +3,8 @@ from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
 
 REPO_URL = 'https://github.com/elihro/obey_testing_goat_superlists.git'
+env.user = 'iro'
+env.host = 'hola'
 
 def deploy():
 	site_folder = f'/home/{env.user}/sites/{env.host}'
