41438c0a4b7f8723681cded85142be9849950264
Jairo Duarte Rodelo <elihro.duarte@gmail.com>
2019-08-13 20:16:43 -0500

    Added creation of folder in fabfile

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,9 @@
+import random
+from fabric.contrib.files import append, exists
+from fabric.api import cd, env, local, run
+
+REPO_URL = 'https://github.com/elihro/obey_testing_goat_superlists.git'
+
+def deploy():
+	site_folder = f'/home/{env.user}/sites/{env.host}'
+	run(f'mkdir -p {site_folder}')
