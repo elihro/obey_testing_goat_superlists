587d7743e03afd75c69f6abbacbe1b6764faa6d2
Jairo Duarte Rodelo <elihro.duarte@gmail.com>
2019-09-02 19:33:41 -0500

    Refactor list view to handle new item POSTs

--- lists/urls.py
+++ lists/urls.py
@@ -4,5 +4,4 @@ from lists import views
 urlpatterns = [
 	url(r'^new$', views.view_new_list, name='view_new_list'),
 	url(r'^(\d+)/$', views.view_list, name='view_list'),
-	url(r'^(\d+)/add_item$', views.add_item, name='view_new_item'),
 ]
--- lists/views.py
+++ lists/views.py
@@ -5,18 +5,15 @@ from django.shortcuts import redirect, render
 from lists.models import Item, List
 
 
-def add_item(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	Item.objects.create(text=request.POST['item_text'], list=list_)
-	return redirect(f'/lists/{list_.id}/')
-	
-	
 def home_page(request):
 	return render(request,'home.html')
 	
 
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'], list=list_)
+		return redirect(f'/lists/{list_.id}/')
 	return render(request, 'list.html',	{'list':list_})
 	
 	
