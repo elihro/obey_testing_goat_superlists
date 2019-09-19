1ddac0806ff8cc927eedf4310008aebd3053b105
Jairo Duarte Rodelo <elihro.duarte@gmail.com>
2019-09-19 12:40:00 -0500

    Use form in all views, back to working state

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -9,7 +9,7 @@ class LayoutAndStylingTest(FuncionalTest):
 		self.browser.set_window_size(1024, 769)
 		
 		# She notices the input box is nicely centered
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.browser.find_element_by_id('id_text')
 		self.assertAlmostEqual(
 			inputbox.location['x'] + inputbox.size['width'] / 2,
 			512,
@@ -21,7 +21,7 @@ class LayoutAndStylingTest(FuncionalTest):
 		inputbox.send_keys('testing')
 		inputbox.send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: testing')
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.browser.find_element_by_id('id_text')
 		self.assertAlmostEqual(
 			inputbox.location['x'] + inputbox.size['width'] /2,
 			512,
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,33 +10,42 @@ class ItemValidationTest(FuncionalTest):
 		# Edith goes to the home page and accidentally tries to submit
 		# an empty list item. She hits Enter on the empty input box
 		self.browser.get(self.live_server_url)
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.browser.find_element_by_id('id_text')
 		inputbox.send_keys(Keys.ENTER)
 		
-		# The home page refreshes, and there is an error message saying
-		# that list items cannot be blank
-		self.wait_for(lambda: self.assertEqual (
-			self.browser.find_element_by_css_selector('.has-error').text,
-			"You can't have an empty list item"
+		# The browser intercepts the request, and does not load the
+		# list page
+		self.wait_for(lambda: self.browser.find_element_by_css_selector (
+			'#id_text:invalid'
 		))
 		
+		# She starts typing some text for the new item and the error disappears
+		inputbox = self.browser.find_element_by_id('id_text')
+		inputbox.send_keys('Buy milk')
+		self.wait_for(lambda: self.browser.find_element_by_css_selector(
+			'#id_text:valid'
+		))
 		
-		# She tries again with some text for the item, which now works
-		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
-		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+		# And she can submit it successfully
+		inputbox = self.browser.find_element_by_id('id_text')
+		inputbox.send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy milk')
-		
+				
 		# Perversely, she now decides to submit a second blank list item
-		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+		self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
 		
-		# She receives a similiar warning on the list page
-		self.wait_for(lambda: self.assertEqual(
-			self.browser.find_element_by_css_selector('.has-error').text,
-			"You can't have an empty list item"
+		# Again, the browser will not comply
+		self.wait_for_row_in_list_table('1: Buy milk')
+		self.wait_for(lambda: self.browser.find_element_by_css_selector(
+			'#id_text:invalid'
 		))
 		
 		# And she can correct it by filling some text in
-		self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
-		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+		inputbox = self.browser.find_element_by_id('id_text')
+		inputbox.send_keys('Make tea')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for(lambda: self.browser.find_element_by_css_selector(
+			'#id_text:valid'
+		))
 		self.wait_for_row_in_list_table('1: Buy milk')
 		self.wait_for_row_in_list_table('2: Make tea')
